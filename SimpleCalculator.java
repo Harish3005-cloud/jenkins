13330855d4f22ce2a01c32ae9b8b3bc6d4c8f699
Harish3005-cloud <harish.e.naidu@gmail.com>
2025-11-02 23:39:57 +0530

    for jenkins

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,54 +1,18 @@
-import java.util.Scanner;
-
+// No Scanner needed
 public class SimpleCalculator {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter first number: ");
-        double num1 = sc.nextDouble();
-
-        System.out.println("Enter second number: ");
-        double num2 = sc.nextDouble();
-
-        System.out.println("Choose an operation (+, -, *, / , %): ");
-        char operator = sc.next().charAt(0);
-
-        double result;
-
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                System.out.println("Result: " + result);
-                break;
-
-            case '-':
-                result = num1 - num2;
-                System.out.println("Result: " + result);
-                break;
-
-            case '*':
-                result = num1 * num2;
-                System.out.println("Result: " + result);
-                break;
-
-            case '/':
-                if (num2 != 0)
-                    result = num1 / num2;
-                else {
-                    System.out.println("Error: Division by zero!");
-                    break;
-                }
-                System.out.println("Result: " + result);
-                break;
-            case '%':
-                result = num1 % num2;
-                System.out.println("Result: " + result);
-                break;
-
-            default:
-                System.out.println("Invalid operator!");
-        }
-
-        sc.close();
+        double num1 = 10.0; // Hard-code a value
+        double num2 = 5.0;  // Hard-code a value
+        
+        // --- Perform your calculations ---
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        
+        // --- Just print the results ---
+        System.out.println("The numbers are: " + num1 + " and " + num2);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        
+        System.out.println("--- Calculation Complete ---");
     }
 }
